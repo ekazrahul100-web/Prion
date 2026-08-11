d4a016790bb50223644ef327a94a65f33f53fe26
ekazrahul100-web <ekazrahul100@gmail.com>
2026-08-12 04:46:01 +0530

    Fix missing HashMap import

--- app/src/main/java/ml/docilealligator/infinityforreddit/activities/ReelsActivity.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/activities/ReelsActivity.java
@@ -44,6 +44,7 @@ import ml.docilealligator.infinityforreddit.utils.NsfwCategoryManager;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
